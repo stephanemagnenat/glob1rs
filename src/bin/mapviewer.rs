6d0b25f50a44d6a504d896b8110fe61cefedd7fc
Stéphane Magnenat <stephane@magnenat.net>
2022-04-23 21:09:31 +0200

    Made indentation consistent.

--- src/bin/mapviewer.rs
+++ src/bin/mapviewer.rs
@@ -9,66 +9,66 @@ struct MapFileName(String);
 fn setup(
     mut commands: Commands,
     mut images: ResMut<Assets<Image>>,
-	mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
     // mut state: ResMut<ViewerState>,
     // windows: ResMut<Windows>,
-	map_file_name: Res<MapFileName>
+    map_file_name: Res<MapFileName>
 ) {
-	// Load terrain images into a texture atlas
-	let glob1images = sprites::load();
-	let mut terrain_atlas_builder = TextureAtlasBuilder::default();
-	let terrain_handles = glob1images
-		.into_iter()
-		.skip(192)
-		.take(164)
-		.map(|image| {
-			let handle = images.add(image);
-			let image = images.get(handle.clone()).unwrap();
-			terrain_atlas_builder.add_texture(handle.clone(), image);
-			handle
-		})
-		.collect::<Vec<_>>();
+    // Load terrain images into a texture atlas
+    let glob1images = sprites::load();
+    let mut terrain_atlas_builder = TextureAtlasBuilder::default();
+    let terrain_handles = glob1images
+        .into_iter()
+        .skip(192)
+        .take(164)
+        .map(|image| {
+            let handle = images.add(image);
+            let image = images.get(handle.clone()).unwrap();
+            terrain_atlas_builder.add_texture(handle.clone(), image);
+            handle
+        })
+        .collect::<Vec<_>>();
     let terrain_atlas = terrain_atlas_builder
-		.finish(&mut images)
-		.unwrap();
+        .finish(&mut images)
+        .unwrap();
 
-	// Create a new tilemap for terrain
-	let map_file_name = &map_file_name.0;
-	let file = File::open(map_file_name).expect("Cannot open map filename");
-	let map = map::load(file).expect("Error reading map");
-	let tiles: Vec<_> = map
-		.into_iter()
-		.enumerate()
-		.flat_map(|(x, col)| {
-			let terrain_atlas = &terrain_atlas;
-			let terrain_handles = &terrain_handles;
-			col.into_iter()
-				.enumerate()
-				.map(move |(y, terrain)| {
-					let sprite_index = terrain_atlas
-						.get_texture_index(&terrain_handles[terrain as usize])
-						.unwrap() as u32
-					;
-					(
-						IVec3::new(x as i32, -(y as i32), 0),
-						Some(Tile { sprite_index, ..Default::default() })
-					)
-				})
-		})
-		.collect();
-	let mut tilemap = TileMap::default();
-	tilemap.set_tiles(tiles);
+    // Create a new tilemap for terrain
+    let map_file_name = &map_file_name.0;
+    let file = File::open(map_file_name).expect("Cannot open map filename");
+    let map = map::load(file).expect("Error reading map");
+    let tiles: Vec<_> = map
+        .into_iter()
+        .enumerate()
+        .flat_map(|(x, col)| {
+            let terrain_atlas = &terrain_atlas;
+            let terrain_handles = &terrain_handles;
+            col.into_iter()
+                .enumerate()
+                .map(move |(y, terrain)| {
+                    let sprite_index = terrain_atlas
+                        .get_texture_index(&terrain_handles[terrain as usize])
+                        .unwrap() as u32
+                    ;
+                    (
+                        IVec3::new(x as i32, -(y as i32), 0),
+                        Some(Tile { sprite_index, ..Default::default() })
+                    )
+                })
+        })
+        .collect();
+    let mut tilemap = TileMap::default();
+    tilemap.set_tiles(tiles);
 
-	// Show terrain
-	let terrain_atlas_handle = texture_atlases.add(terrain_atlas);
-	let terrain_bundle = TileMapBundle {
-		tilemap,
-		texture_atlas: terrain_atlas_handle,
+    // Show terrain
+    let terrain_atlas_handle = texture_atlases.add(terrain_atlas);
+    let terrain_bundle = TileMapBundle {
+        tilemap,
+        texture_atlas: terrain_atlas_handle,
         ..Default::default()
     };
-	let mut camera = OrthographicCameraBundle::new_2d();
-	camera.transform.translation = Vec3::new(512.0 * 32.0, -512.0 * 32.0, 0.0);
-	commands.spawn_bundle(camera);
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.transform.translation = Vec3::new(512.0 * 32.0, -512.0 * 32.0, 0.0);
+    commands.spawn_bundle(camera);
     commands.spawn_bundle(terrain_bundle);
 }
 
@@ -78,24 +78,24 @@ fn input_system(
     mut camera_transform_query: Query<(&mut Transform,), With<Camera2d>>,
     // mut tilemap_visible_query: Query<&mut Visibility, With<TileMap>>,
     keyboard_input: Res<Input<KeyCode>>,
-	mut scroll_evr: EventReader<MouseWheel>,
-	mut motion_evr: EventReader<MouseMotion>,
-	buttons: Res<Input<MouseButton>>,
+    mut scroll_evr: EventReader<MouseWheel>,
+    mut motion_evr: EventReader<MouseMotion>,
+    buttons: Res<Input<MouseButton>>,
     time: Res<Time>,
 ) {
     if let Some(active_camera_entity) = active_camera.get() {
         if let Ok((mut tf,)) = camera_transform_query.get_mut(active_camera_entity) {
-			let move_speed = 1000.0 * tf.scale.x;
-			let zoom_speed = 5.0 * tf.scale.x;
+            let move_speed = 1000.0 * tf.scale.x;
+            let zoom_speed = 5.0 * tf.scale.x;
 
             if keyboard_input.pressed(KeyCode::PageUp) {
-				let amount = zoom_speed * time.delta_seconds();
+                let amount = zoom_speed * time.delta_seconds();
                 tf.scale.x = (tf.scale.x - amount).max(0.1);
-				tf.scale.y = (tf.scale.y - amount).max(0.1);
+                tf.scale.y = (tf.scale.y - amount).max(0.1);
             } else if keyboard_input.pressed(KeyCode::PageDown) {
                 let amount = zoom_speed * time.delta_seconds();
                 tf.scale.x += amount;
-				tf.scale.y += amount;
+                tf.scale.y += amount;
             }
 
             if keyboard_input.pressed(KeyCode::Left) {
@@ -111,35 +111,35 @@ fn input_system(
             }
 
             use bevy::input::mouse::MouseScrollUnit;
-			for ev in scroll_evr.iter() {
-				let factor = match ev.unit {
-					MouseScrollUnit::Line => ev.y * 12.0,
-					MouseScrollUnit::Pixel => ev.y
-				};
-				let amount = -zoom_speed * factor * 0.002;
-				tf.scale.x = (tf.scale.x + amount).max(0.1);
-				tf.scale.y = (tf.scale.y + amount).max(0.1);
-			}
-			if buttons.pressed(MouseButton::Middle) {
-				for ev in motion_evr.iter() {
-					tf.translation.x -= ev.delta.x * tf.scale.x;
-					tf.translation.y += ev.delta.y * tf.scale.y;
-				}
-			}
+            for ev in scroll_evr.iter() {
+                let factor = match ev.unit {
+                    MouseScrollUnit::Line => ev.y * 12.0,
+                    MouseScrollUnit::Pixel => ev.y
+                };
+                let amount = -zoom_speed * factor * 0.002;
+                tf.scale.x = (tf.scale.x + amount).max(0.1);
+                tf.scale.y = (tf.scale.y + amount).max(0.1);
+            }
+            if buttons.pressed(MouseButton::Middle) {
+                for ev in motion_evr.iter() {
+                    tf.translation.x -= ev.delta.x * tf.scale.x;
+                    tf.translation.y += ev.delta.y * tf.scale.y;
+                }
+            }
         }
     }
 }
 
 fn main() {
-	let file_name = std::env::args().nth(1).expect("Missing map filename");
+    let file_name = std::env::args().nth(1).expect("Missing map filename");
 
-	App::new()
-		// Disable MSAA, as it produces weird rendering artifacts
+    App::new()
+        // Disable MSAA, as it produces weird rendering artifacts
         .insert_resource(Msaa { samples: 1 })
-		.add_plugins(DefaultPlugins)
-		.add_plugin(SimpleTileMapPlugin)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(SimpleTileMapPlugin)
         .insert_resource(MapFileName(file_name))
-		.add_system(input_system)
-		.add_startup_system(setup)
+        .add_system(input_system)
+        .add_startup_system(setup)
         .run();
 }
